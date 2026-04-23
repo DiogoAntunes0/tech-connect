725f4fc91e740930ff48c17073b3658466af43c9
Diogo Antunes <diogoantunes892@gmail.com>
2026-04-23 20:35:25 -0300

    iniciando backend

---
+++ backend/src/main/java/br/com/tech_connect/backend/TechConnectApplication.java
@@ -0,0 +1,14 @@
+package br.com.tech_connect.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechConnectApplication.class, args);
+
+	}
+
+}
